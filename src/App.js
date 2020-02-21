b6425ac103334b4a2363b6cf5411303974a5679c
unknown <josephabarentos@gmail.com>
2020-02-20 20:14:48 -0800

    added the increment, decrement and clear methods

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React, {Component} from 'react';
 
 class Counter extends Component {
+constructor() {
+  super();
+  this.state = {
+    count: 0,
+  };
+}
+
+//this is where our methods go
+increment = () => {
+  this.setState ({
+    count: this.state.count + 1
+  })
+}
+
+decrement = () => {
+  this.setState ({
+    count: this.state.count - 1
+  })
+}
+
+clear = () => {
+  this.setState ({
+    count: this.state.count = 0
+  })
+}
+
 
   render() {
-    return {
-      <div></div>
-    }
+    return (
+
+      <div className="container">
+        <div className="navbar">Counter</div>
+        <div className="counter">
+          <h1>{this.state.count}</h1>
+          <button type="button" onClick={this.clear}>Clear</button>
+          <button type="button" onClick={this.increment}>Increment</button>
+          <button type="button" onClick={this.decrement}>Decrement</button>
+
+      </div>
+      </div>
+    );
   }
 }
 
